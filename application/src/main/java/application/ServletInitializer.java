fbe49962fc58979540e9cbe43d31dfb3279f4ce8
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-06-25 23:18:47 +0630

    Ready To Start Issue Manager Application

---
+++ application/src/main/java/application/ServletInitializer.java
@@ -0,0 +1,16 @@
+package application;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author Hein Htet Aung
+ * @created at 25/06/2022
+ **/
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(IssueManagerApplication.class);
+    }
+
+}
